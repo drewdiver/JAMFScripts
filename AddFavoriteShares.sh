27009821dbb223f166d67f8fe14fbd7e41fb4ba1
Drew Diver <shout@drewdiver.com>
2017-06-20 15:41:11 +0200

    Script to auto-populate favorite shares.

---
+++ AddFavoriteShares.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+#Title:			 AddFavoriteShares.sh
+#Description:	 This will populate a users "Connect to Server" 
+#				 menu with the servers you specify.
+#Author:		 Drew Diver
+#Date:			 2017-06-20
+#==================================================================
+
+# CONSTANTS
+sfltool=/usr/bin/sfltool
+plist=com.apple.LSSharedFileList.FavoriteServers
+
+# SUPPRESS ERRORS FROM JSS
+exec 2>/dev/null
+
+# ADD SHARES TO "FAVORITE SERVERS" LIST UNDER "CONNECT TO SERVER"
+$sfltool add-item -n "First Share Name" $plist "smb://my.first.share/public"
+$sfltool add-item -n "Second Share Name" $plist "smb://my.second.share/tmp"
