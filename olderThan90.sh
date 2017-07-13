bd5a49fbc62ec44e498f9751dfb02e3dbba7934b
Drew Diver <shout@drewdiver.com>
2017-07-13 12:36:55 +0200

    Added removal of home folder

--- olderThan90.sh
+++ olderThan90.sh
@@ -6,7 +6,10 @@ userArray=$(find /Users ! -name 'Shared' -type d -mtime +90 -maxdepth 1)
 for user in "${userArray[@]}"
 do
     echo "Deleting user: $user..."
+    # Delete the account from the local user database
     dscl . -delete $user
+    # Delete the accounts home folder
+    rm -rf /Users/$user
     ((userCount += 1))
 done
 
